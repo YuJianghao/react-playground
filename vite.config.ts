cf09137af3b7d50afb2e4c37841f305ec6fbea46
winwin2011 <yjh2011@live.com>
2022-06-11 23:32:42 +0800

    chore: build to docs

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 import svgr from '@honkhonk/vite-plugin-svgr'
 
 export default defineConfig({
+  build: {
+    outDir: './docs',
+  },
   plugins: [
     svgr(),
     react({
