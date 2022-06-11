1ad032214e59b060626f1be929447ac3685f5785
winwin2011 <yjh2011@live.com>
2022-06-11 18:08:08 +0800

    chore: alert panel

--- src/views/AlertCenter.tsx
+++ src/views/AlertCenter.tsx
@@ -15,6 +15,14 @@ export const AlertCenter: React.FC = () => {
     >
       <Breadcrumb />
       <PageTitle>Alert Center</PageTitle>
+      <div
+        css={{
+          boxShadow: '0px 10px 64px rgba(0, 0, 0, 0.06)',
+          padding: '24px',
+          backgroundColor: '#fff',
+          borderRadius: '8px',
+        }}
+      ></div>
     </div>
   )
 }
