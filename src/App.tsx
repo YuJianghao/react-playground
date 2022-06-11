69b3b941921f6723d9b4870c5cf5993395fc07f8
winwin2011 <yjh2011@live.com>
2022-06-11 08:42:30 +0800

    chore: emotion css

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,20 @@ import { Link, Outlet } from 'react-router-dom'
 export const App: React.FC = () => {
   return (
     <>
-      <h1>hi from React router</h1>
+      <h1>
+        hi from React with {' '}
+        <span
+          css={{
+            color: 'red',
+          }}
+        >
+          Emotion CSS
+        </span>
+      </h1>
       <nav>
         <Link to="/page1">page1</Link> - <Link to="/page2">page2</Link>
       </nav>
-      <Outlet/>
+      <Outlet />
     </>
   )
 }
